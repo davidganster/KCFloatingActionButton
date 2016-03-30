97955385dd7f8b82aa4cf00347e2c994b84789a2
David Ganster <david.ganster@wirecube.at>
2016-03-30 11:36:29 +0200

    aspect fit

--- KCFloatingActionButton/KCFloatingActionButtonItem.swift
+++ KCFloatingActionButton/KCFloatingActionButtonItem.swift
@@ -92,7 +92,7 @@ public class KCFloatingActionButtonItem: UIView {
                 _iconImageView = UIImageView(frame: CGRectMake(0, 0, 21, 23))
                 //                _iconImageView = UIImageView(frame: CGRectMake(frame.size.width - size, 0, 21, 23))
                 _iconImageView?.center = CGPointMake(size/2, size/2)
-                _iconImageView?.contentMode = UIViewContentMode.ScaleToFill
+                _iconImageView?.contentMode = UIViewContentMode.ScaleAspectFit
                 addSubview(_iconImageView!)
             }
             return _iconImageView!
